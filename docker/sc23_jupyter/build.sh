52830171745f1050b12fa8160acc4edbf08c93d9
Cyrus Harrison <cyrush@llnl.gov>
2023-11-01 09:53:44 -0700

    docker recipe updates

--- docker/sc23_jupyter/build.sh
+++ docker/sc23_jupyter/build.sh
@@ -3,11 +3,11 @@
 # Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
 # other details. No copyright assignment is required to contribute to Ascent.
 
-export TAG_BASE=alpinedav/ascentascent-ubuntu-22.04_2023-10-26-shadfd6a2
+export TAG_BASE=alpinedav/ascent:sc23_jupyter
 
 date
 
-python3 build_and_tag.py ${TAG_BASE}
+python build_and_tag.py ${TAG_BASE}
 
 date
 
--- docker/sc23_jupyter/docker_catalyst_build.sh
+++ docker/sc23_jupyter/docker_catalyst_build.sh
@@ -11,9 +11,32 @@ export PATH=`pwd`/cmake-3.27.7-linux-x86_64/bin/:$PATH
 # catalyst source, build, and install
 curl -L https://gitlab.kitware.com/api/v4/projects/5912/packages/generic/catalyst/v2.0.0-rc4/catalyst-v2.0.0-rc4.tar.gz -o catalyst-v2.0.0-rc4.tar.gz
 tar -xvf catalyst-v2.0.0-rc4.tar.gz
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/catalyst-v2.0.0-rc4-install -DCATALYST_USE_MPI=ON -DCATALYST_WRAP_PYTHON=ON -B catalyst-v2.0.0-rc4-build -S catalyst-v2.0.0-rc4
-cmake --build catalyst-v2.0.0-rc4-build
+cmake -DCMAKE_INSTALL_PREFIX=/catalyst-v2.0.0-rc4-install -DCATALYST_USE_MPI=ON -DCATALYST_WRAP_PYTHON=ON -B catalyst-v2.0.0-rc4-build -S catalyst-v2.0.0-rc4
+cmake --build catalyst-v2.0.0-rc4-build -j
 cmake --install catalyst-v2.0.0-rc4-build
 
+# install osmesa
+apt-get update && apt-get -y install libosmesa6 libosmesa6-dev
+
+# download and build paraview
+curl -L "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.11&type=source&os=Sources&downloadFile=ParaView-v5.11.2.tar.gz" -o ParaView-v5.11.2.tar.gz
+tar -xzf ParaView-v5.11.2.tar.gz
+
+cmake -DVTK_OPENGL_HAS_OSMESA=ON \
+      -DVTK_USE_X=OFF \
+      -DVTK_MODULE_ENABLE_ParaView_InSitu=YES \
+      -DPARAVIEW_ENABLE_CATALYST=YES \
+      -DPARAVIEW_ENABLE_EXAMPLES=YES \
+      -DPARAVIEW_USE_PYTHON=ON \
+      -DPARAVIEW_USE_QT=OFF \
+      -DPARAVIEW_USE_VTKM=OFF \
+      -Dcatalyst_DIR=`pwd`/catalyst-v2.0.0-rc4-install \
+      -S ParaView-v5.11.2 \
+      -B ParaView-v5.11.2-build \
+      -DCMAKE_INSTALL_PREFIX=/ParaView-v5.11.2-install
+cmake --build ParaView-v5.11.2-build -j 6
+cmake --install ParaView-v5.11.2-build
+
 # create script we can use for env setup to use catalyst via python
-echo "export PYTHONPATH=$PYTHONPATH:`pwd`/catalyst-v2.0.0-rc4-install/lib/python3.9/site-packages/" >> catalyst_python_setup.sh
+echo "export PYTHONPATH=$PYTHONPATH:`pwd`/catalyst-v2.0.0-rc4-install/lib/python3.9/site-packages/" >> catalyst_docker_setup_env.sh
+echo "export CATALYST_IMPLEMENTATION_PATHS=/ParaView-v5.11.2-install/lib/catalyst/" >> catalyst_docker_setup_env.sh
